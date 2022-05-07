0c74340f9f12c8196696a0369b49b12ddb6095a0
Orlina M Brabar <90068655+OrLina1125@users.noreply.github.com>
2022-05-07 09:56:30 +0900

    add schedule

---
+++ index.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <title>Exercise schedule</title>
+    <link rel="stylesheet" type="text/css" href="style.css">
+</head>
+
+<body>
+
+<?php
+    $dayofweek = date("w");
+    //echo $dayofweek;
+
+    switch ($dayofweek){
+        case 1:
+            echo "It is Monday!";
+            break;
+        case 2:
+            echo "It is Tuesday!";
+            break;   
+        case 3:
+            echo "It is Wednesday!";
+            break;
+        case 4:
+            echo "It is Thursday!";
+            break;
+        case 5:
+            echo "It is Friday!";
+            break;
+        case 6:
+            echo "<p>It is Saturday!</p>";
+            break;
+        case 0:
+            echo "It is Sunday!";
+            break;
+
+
+    }
+
+?>
+</body>
+</html>
+
